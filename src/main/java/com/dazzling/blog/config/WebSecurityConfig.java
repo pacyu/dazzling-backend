11fc1e80f6e4ff1383a921ee069780586d42e270
darkchii <darkchii@qq.com>
2026-05-28 20:33:46 +0800

    update

--- src/main/java/com/dazzling/blog/config/WebSecurityConfig.java
+++ src/main/java/com/dazzling/blog/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,9 +28,6 @@ public class WebSecurityConfig {
 	
 	@Autowired
 	private CustomUserDetailsService userDetailsService;
-
-	@Autowired
-  private PasswordEncoder passwordEncoder;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -90,15 +88,9 @@ public class WebSecurityConfig {
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
-			auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-			return auth.build();
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
-
-	// @Bean
-	// public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-	// 	return config.getAuthenticationManager();
-	// }
 
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
